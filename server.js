574e2ea6a964447cae1fb6094bc0c0246dbf24b6
Didier Arcucci <didierarcucci@Didiers-MacBook-Pro.local>
2017-05-07 13:50:42 -0500

    add proxy request

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // server.js
 const express = require('express');
+const request = require('request');
 const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+app.use('/MED.Service', function(req, res) {
+    var url = 'https://stageservices.essilorlink.com/Inventory/V_C/AvailableQuantityService';
+    req.pipe(request(url)).pipe(res);
+})
 
 // Start the app by listening on the default
 // Heroku port
